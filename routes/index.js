772d088178ac9b4a1d34ba728050391f1f74dded
ChumachI <94695810+ChumachI@users.noreply.github.com>
2022-06-21 15:19:38 +0300

    hotfix/настроить ответ при неправильном пути

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 
 const cardsRouter = require('./card.js');
 const usersRouter = require('./user.js');
+const not_found = require('./not-found');
 
 router.use(
   usersRouter,
   cardsRouter,
+  not_found
 );
 
 module.exports = router;
