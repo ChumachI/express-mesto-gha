bf569f30e7fc360d488c0380b34dd39aa0b723f3
ChumachI <94695810+ChumachI@users.noreply.github.com>
2022-06-21 15:32:21 +0300

    hotfix/исправить ответ при неправильном пути - 2

--- routes/not-found.js
+++ routes/not-found.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const {ERROR_NOT_FOUND} = require('../utils/constants')
 
 router.all("*", (req, res) => {
   res.status(ERROR_NOT_FOUND).send({ message: "Страница не найдена" });
