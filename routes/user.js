b756ff21dd2efd95df13587233cb8f4094906e40
ChumachI <94695810+ChumachI@users.noreply.github.com>
2022-06-28 11:02:59 +0300

    hotfix/Очередные исправления

--- routes/user.js
+++ routes/user.js
@@ -3,10 +3,10 @@ const {
   getAllUsers, getOneUser, createUser, updateMyInfo, updateMyAvatar, login, getMyInfo,
 } = require('../controllers/users');
 const auth = require('../middlewares/auth');
-const { checkUser, checkUserId, checkAvatar } = require('../utils/validation');
+const { checkUser, checkAvatar } = require('../utils/validation');
 
 router.get('/users', auth, getAllUsers);
-router.get('/users/:userId', auth, checkUserId, getOneUser);
+router.get('/users/:userId', auth, getOneUser);
 router.get('/users/me', auth, getMyInfo);
 router.post('/signin', checkUser, login);
 router.post('/signup', checkUser, createUser);
