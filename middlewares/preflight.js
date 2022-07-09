4315a3a6bc93e4ef185a9a58b7111f74e63385f3
ChumachI <94695810+ChumachI@users.noreply.github.com>
2022-07-09 18:36:23 +0300

    hotfix/ доработать обработку предварительных запросов

--- middlewares/preflight.js
+++ middlewares/preflight.js
@@ -1,9 +1,11 @@
 const preflight = (req, res, next) => {
   const { method } = req;
+  const { origin } = req.headers;
   if (method === 'OPTIONS') {
     const requestHeaders = req.headers['access-control-request-headers'];
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Origin', origin);
     res.end();
   }
   next();
