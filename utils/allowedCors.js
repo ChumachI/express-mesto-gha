5fe64b4edc225bd7c3dfa5f2494d96189dabecce
ChumachI <94695810+ChumachI@users.noreply.github.com>
2022-07-13 01:33:34 +0300

    hotfix/использовать библиотеку cors - 18

--- utils/allowedCors.js
+++ utils/allowedCors.js
@@ -1,7 +1,7 @@
 const allowedCors = [
   'https://mesto.ilya.chumak.nomoredomains.xyz',
   'http://mesto.ilya.chumak.nomoredomains.xyz',
-  'localhost:3000',
+  'http://localhost:3000',
 ];
 
 module.exports = { allowedCors };
