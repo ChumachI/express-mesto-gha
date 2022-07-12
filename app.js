3cf90c68efda2ba1c28557e21fac85400a55a5a3
ChumachI <94695810+ChumachI@users.noreply.github.com>
2022-07-12 16:42:46 +0300

    hotfix/использовать библиотеку cors - 8

--- app.js
+++ app.js
@@ -14,10 +14,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({
-  origin: 'http://mesto.ilya.chumak.nomoredomains.xyz',
-  'Access-Control-Allow-Origin': '*',
-}));
+app.use(cors);
 app.use(router);
 app.use(errors());
 app.use((err, req, res, next) => {
