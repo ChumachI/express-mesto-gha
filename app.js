1df61ec8d6f8458f296446d2dbc9a51d01517dc0
ChumachI <94695810+ChumachI@users.noreply.github.com>
2022-07-12 15:12:34 +0300

    hotfix/использовать библиотеку cors - 4

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.options('*', cors(corsConfig));
-app.use(cors(corsConfig));
+app.use(cors());
 app.use(router);
 app.use(errors());
 app.use((err, req, res, next) => {
