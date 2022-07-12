5f163fff0bfeae00b2c08f3cc246aa0e6f55a0eb
ChumachI <94695810+ChumachI@users.noreply.github.com>
2022-07-13 01:09:39 +0300

    hotfix/использовать библиотеку cors - 16

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const cors = require('cors');
-const allowedCors = require('./utils/allowedCors');
 
 const app = express();
 const router = require('./routes/index');
@@ -12,17 +11,7 @@ const router = require('./routes/index');
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
-app.use(cors({
-  origin(origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedCors.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not '
-                + 'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-}));
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
