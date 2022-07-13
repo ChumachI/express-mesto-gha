ac4a4786151b42f270d1b8a5b58f212d2e4261a5
ChumachI <94695810+ChumachI@users.noreply.github.com>
2022-07-13 12:41:30 +0300

    hotfix/использовать ошибки авторизации

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -11,7 +11,7 @@ module.exports = (req, res, next) => {
   try {
     playload = jwt.verify(token, 'some-secret-key');
   } catch (e) {
-    const err = new UnauthorizedError('Необходима авторизация');
+    const err = new UnauthorizedError('Необходима авторизация' + `${authorization}`);
 
     next(err);
   }
